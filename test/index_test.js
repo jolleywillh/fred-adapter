de905aa8e480d46e95dfc8bf77ecc4d960e56b96
jolleywillh <20263366+jolleywillh@users.noreply.github.com>
2021-05-29 15:39:51 -0700

    First commit

---
+++ test/index_test.js
@@ -0,0 +1,48 @@
+const assert = require('chai').assert
+const dotenv = require('dotenv').config()
+const createRequest = require('../index.js').createRequest
+
+describe('createRequest', () => {
+  const jobID = '1'
+
+  context('successful calls', () => {
+    const requests = [
+      { name: 'id not supplied', testData: { data: { data_series: 'gnpca' } } },
+      { name: 'data_series', testData: { id: jobID, data: { data_series: 'gnpca' } } },
+    ]
+
+    requests.forEach(req => {
+      it(`${req.name}`, (done) => {
+        createRequest(req.testData, (statusCode, data) => {
+          assert.equal(statusCode, 200)
+          assert.equal(data.jobRunID, jobID)
+          assert.isNotEmpty(data.data)
+          assert.isAbove(Number(data.result), 0)
+          assert.isAbove(Number(data.data.result), 0)
+          done()
+        })
+      })
+    })
+  })
+
+  context('error calls', () => {
+    const requests = [
+      { name: 'empty body', testData: {} },
+      { name: 'empty data', testData: { data: {} } },
+      { name: 'series not supplied', testData: { id: jobID, data: { series_error: 'gnpca' } } },
+      { name: 'unknown series', testData: { id: jobID, data: { data_series: 'not_real'} } },
+    ]
+
+    requests.forEach(req => {
+      it(`${req.name}`, (done) => {
+        createRequest(req.testData, (statusCode, data) => {
+          assert.equal(statusCode, 500)
+          assert.equal(data.jobRunID, jobID)
+          assert.equal(data.status, 'errored')
+          assert.isNotEmpty(data.error)
+          done()
+        })
+      })
+    })
+  })
+})
